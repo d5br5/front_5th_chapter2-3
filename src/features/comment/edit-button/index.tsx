34349cd1ee828e1871120f0be39ae7a62e0e56a9
DohyungKim <ehgud456456@naver.com>
2025-05-02 08:50:35 +0900

    fix: 댓글 수정 버튼 재활성화

--- src/features/comment/edit-button/index.tsx
+++ src/features/comment/edit-button/index.tsx
@@ -2,16 +2,22 @@ import { Edit2 } from "lucide-react"
 
 import { Comment } from "@/entity/comment/model/types"
 import { Button } from "@/shared/ui"
+import { useDialogStore } from "@/features/dialog/model/store"
+import { EDIT_COMMENT_DIALOG } from "../edit-dialog"
+import { useSelectedComment } from "@/entity/comment/model/selectedComment"
 
 export const CommentEditButton = ({ comment }: { comment: Comment }) => {
+  const { setDialogOpen } = useDialogStore()
+  const { setSelectedComment } = useSelectedComment()
+
   return (
     <Button
       variant="ghost"
       size="sm"
-      // onClick={() => {
-      //   setSelectedComment(comment)
-      //   setShowEditCommentDialog(true)
-      // }}
+      onClick={() => {
+        setSelectedComment(comment)
+        setDialogOpen(EDIT_COMMENT_DIALOG, true)
+      }}
     >
       <Edit2 className="w-3 h-3" />
     </Button>
