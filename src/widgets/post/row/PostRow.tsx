7be162015952dc30421150f01de95b66e8d749f5
DohyungKim <ehgud456456@naver.com>
2025-05-02 08:59:07 +0900

    refactor: tag badge 컴포넌트 분리

--- src/widgets/post/row/PostRow.tsx
+++ src/widgets/post/row/PostRow.tsx
@@ -6,14 +6,13 @@ import { highlightText } from "@/shared/lib/highlightText"
 
 import { useDialogStore } from "@/features/dialog/model/store"
 
-import { useSelectedTag } from "@/entity/tag/model/selectedTag"
-
 import { POST_DETAIL_DIALOG } from "@/widgets/post/detail-dialog"
 import { UserPost } from "@/entity/post/model/useUserPosts"
 import { useSelectedPost } from "@/entity/post/model/selectedPost"
 import { useSelectedUser } from "@/entity/user/model/selectedUser"
 import { PostEditButton } from "@/features/post/edit-button"
 import { PostDeleteButton } from "@/features/post/delete-button"
+import { TagBadge } from "@/features/tag/badge"
 
 interface PostRowProps {
   post: UserPost
@@ -23,7 +22,6 @@ export const PostRow = ({ post }: PostRowProps) => {
   const { searchQuery } = useSearchQueryStore()
   const { setSelectedPost } = useSelectedPost()
   const { setDialogOpen } = useDialogStore()
-  const { selectedTag, setSelectedTag } = useSelectedTag()
   const { setSelectedUser } = useSelectedUser()
 
   // 게시물 상세 보기
@@ -38,25 +36,7 @@ export const PostRow = ({ post }: PostRowProps) => {
       <TableCell>
         <div className="space-y-1">
           <div>{highlightText(post.title, searchQuery)}</div>
-
-          <div className="flex flex-wrap gap-1">
-            {post.tags?.map((tag) => (
-              <span
-                key={tag}
-                className={`px-1 text-[9px] font-semibold rounded-[4px] cursor-pointer ${
-                  selectedTag === tag
-                    ? "text-white bg-blue-500 hover:bg-blue-600"
-                    : "text-blue-800 bg-blue-100 hover:bg-blue-200"
-                }`}
-                onClick={() => {
-                  setSelectedTag(tag)
-                  //   updateURL()
-                }}
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
+          <div className="flex flex-wrap gap-1">{post.tags?.map((tag) => <TagBadge key={tag} tag={tag} />)}</div>
         </div>
       </TableCell>
       <TableCell>
