548fb532571854e045c14558586fb5f88f0316f5
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:35:16 +0900

    feat: 게시물 삭제 기능

--- src/features/post/delete/index.tsx
+++ src/features/post/delete/index.tsx
@@ -1,12 +1,22 @@
 import { deletePost } from "@/entity/post/api/deletePost"
+import { GetPostsResponse } from "@/entity/post/model/types"
 import { PostDeleteButton } from "@/entity/post/ui/DeleteButton"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export const PostDelete = ({ postId }: { postId: number }) => {
+  const queryClient = useQueryClient()
+
   const { mutate } = useMutation({
     mutationFn: () => deletePost(postId),
     onSuccess: () => {
       // 기존 포스트 목록에서 제거
+      queryClient.setQueriesData({ queryKey: ["posts"] }, (old: GetPostsResponse) => {
+        if (!old) return
+        return {
+          ...old,
+          posts: old.posts.filter((post) => post.id !== postId),
+        }
+      })
     },
     onError: (error) => {
       console.error("게시물 삭제 오류:", error)
