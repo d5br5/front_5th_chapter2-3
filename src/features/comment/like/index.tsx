a218bb7d36aee99ddc5fa992c0f94a6a83bf7908
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:42:18 +0900

    feat: 댓글 좋아요 기능

--- src/features/comment/like/index.tsx
+++ src/features/comment/like/index.tsx
@@ -1,20 +1,21 @@
 import { likeComment } from "@/entity/comment/api/likeComment"
-import { Comment } from "@/entity/comment/model/types"
+import { Comment, GetCommentsResponse } from "@/entity/comment/model/types"
 import { CommentLikeButton } from "@/entity/comment/ui/LikeButton"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export const CommentLike = ({ comment }: { comment: Comment }) => {
+  const queryClient = useQueryClient()
   const { mutate } = useMutation({
     mutationFn: () => likeComment(comment.id, comment.likes + 1),
     onSuccess: (data) => {
-      // TODO: 댓글 좋아요 업데이트
-      console.log(data)
-      //     setComments((prev) => ({
-      //       ...prev,
-      //       [postId]: prev[postId].map((comment) =>
-      //         comment.id === data.id ? { ...data, likes: comment.likes + 1 } : comment,
-      //       ),
-      //     }))
+      // 댓글 좋아요 업데이트
+      queryClient.setQueryData(["comments", comment.postId], (old: GetCommentsResponse) => {
+        if (!old) return
+        return {
+          ...old,
+          comments: old.comments.map((c) => (c.id === data.id ? { ...data, likes: comment.likes + 1 } : c)),
+        }
+      })
     },
     onError: (error) => {
       console.error("댓글 좋아요 오류:", error)
