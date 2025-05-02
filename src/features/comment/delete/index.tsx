628a181b6e39745beee61a95d93cf09ec861db8e
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:31:03 +0900

    feat: 댓글 삭제 기능

--- src/features/comment/delete/index.tsx
+++ src/features/comment/delete/index.tsx
@@ -1,18 +1,22 @@
 import { deleteComment } from "@/entity/comment/api/deleteComment"
-import { Comment } from "@/entity/comment/model/types"
+import { Comment, GetCommentsResponse } from "@/entity/comment/model/types"
 import { CommentDeleteButton } from "@/entity/comment/ui/DeleteButton"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 export const CommentDelete = ({ comment }: { comment: Comment }) => {
+  const queryClient = useQueryClient()
+
   const { mutate } = useMutation({
     mutationFn: () => deleteComment(comment.id),
     onSuccess: () => {
-      console.log(comment.postId)
       // 기존 댓글 목록에서 제거
-      //setComments((prev) => ({
-      //       ...prev,
-      //       [postId]: prev[postId].filter((comment) => comment.id !== id),
-      //     }))
+      queryClient.setQueryData(["comments", comment.postId], (old: GetCommentsResponse) => {
+        if (!old) return
+        return {
+          ...old,
+          comments: old.comments.filter((c) => c.id !== comment.id),
+        }
+      })
     },
     onError: (error) => {
       console.error("댓글 삭제 오류:", error)
