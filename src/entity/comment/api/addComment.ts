d49e0e296d4a6dca6f494ec4520e75c6c0de9657
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:40:10 +0900

    feat: 댓글 추가 기능

--- src/entity/comment/api/addComment.ts
+++ src/entity/comment/api/addComment.ts
@@ -1,7 +1,7 @@
 import { NewComment } from "../model/types"
 
 export const addComment = async (comment: NewComment) => {
-  const response = await fetch("/api/comments/add", {
+  const response = await fetch(`/api/comments/add`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(comment),
--- src/widgets/comment/add-dialog/index.tsx
+++ src/widgets/comment/add-dialog/index.tsx
@@ -1,26 +1,31 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { addComment } from "@/entity/comment/api/addComment"
 import { Button, Dialog, DialogContent, DialogHeader, DialogTitle, Textarea } from "@/shared/ui"
 import { useDialogStore } from "@/features/dialog/model/store"
 import { useNewComment } from "@/features/comment/add/model/useNewComment"
+import { GetCommentsResponse } from "@/entity/comment/model/types"
+import { useSelectedPost } from "@/entity/post/model/selectedPost"
 
 export const ADD_COMMENT_DIALOG = "ADD_COMMENT_DIALOG"
 
 export const AddCommentDialog = () => {
+  const queryClient = useQueryClient()
+  const { selectedPost } = useSelectedPost()
   const { isDialogOpen, setDialogOpen, closeDialog } = useDialogStore()
   const { newComment, setBody, reset } = useNewComment()
 
   const mutation = useMutation({
-    mutationFn: () => addComment(newComment),
+    mutationFn: () => addComment({ ...newComment, postId: selectedPost?.id }),
     onSuccess: (data) => {
       reset()
       closeDialog(ADD_COMMENT_DIALOG)
-      console.log(data)
-      // 기존 코멘트 목록에 추가
-      //   setComments((prev) => ({
-      //     ...prev,
-      //     [data.postId]: [...(prev[data.postId] || []), data],
-      //   }))
+      queryClient.setQueryData(["comments", data.postId], (old: GetCommentsResponse) => {
+        if (!old) return
+        return {
+          ...old,
+          comments: [data, ...old.comments],
+        }
+      })
     },
     onError: (error) => {
       console.error("댓글 추가 오류:", error)
