6e17515a155998cd538d16583c720d202cc78450
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:32:25 +0900

    feat: 쿼리 유효기간 10분 지정

--- src/app/providers/QueryClientProvider.tsx
+++ src/app/providers/QueryClientProvider.tsx
@@ -2,7 +2,14 @@ import { useState } from "react"
 import { QueryClient, QueryClientProvider as ReactQueryClientProvider } from "@tanstack/react-query"
 
 export const QueryClientProvider = ({ children }: { children: React.ReactNode }) => {
-  const [queryClient] = useState(() => new QueryClient())
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: { refetchOnWindowFocus: false, staleTime: 10 * 60_000 },
+        },
+      }),
+  )
 
   return <ReactQueryClientProvider client={queryClient}>{children}</ReactQueryClientProvider>
 }
