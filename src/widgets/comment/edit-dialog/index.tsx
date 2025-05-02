acba2726df04dc4cf57341248108b9c148668cf6
Doh Kim <ehgud456456@naver.com>
2025-05-02 11:41:17 +0900

    feat: 댓글 수정 기능

--- src/widgets/comment/edit-dialog/index.tsx
+++ src/widgets/comment/edit-dialog/index.tsx
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 
 import { Button, Dialog, DialogContent, DialogHeader, DialogTitle, Textarea } from "@/shared/ui"
 import { useDialogStore } from "@/features/dialog/model/store"
 import { useSelectedComment } from "@/entity/comment/model/selectedComment"
 import { useEditComment } from "@/features/comment/edit/model/useEditComment"
 import { editComment } from "@/entity/comment/api/editComment"
+import { GetCommentsResponse } from "@/entity/comment/model/types"
 
 export const EDIT_COMMENT_DIALOG = "EDIT_COMMENT_DIALOG"
 
 export const EditCommentDialog = () => {
+  const queryClient = useQueryClient()
   const { isDialogOpen, setDialogOpen, closeDialog } = useDialogStore()
   const { selectedComment, setSelectedComment } = useSelectedComment()
   const { body, setBody } = useEditComment()
@@ -21,7 +23,13 @@ export const EditCommentDialog = () => {
       setSelectedComment(null)
       closeDialog(EDIT_COMMENT_DIALOG)
       // 댓글 목록 수정
-      console.log(data)
+      queryClient.setQueryData(["comments", selectedComment?.postId], (old: GetCommentsResponse) => {
+        if (!old) return
+        return {
+          ...old,
+          comments: old.comments.map((c) => (c.id === data.id ? data : c)),
+        }
+      })
     },
     onError: (error) => {
       console.error("댓글 수정 오류:", error)
